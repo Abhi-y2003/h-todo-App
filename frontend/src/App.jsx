7fbaf4f3048baa88a81b2c1afc64cf60cd4c38dc
Abhi-y2003 <abhisheks2651@gmail.com>
2024-07-22 11:01:37 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {CreateTodo} from './components/CreateTodo'
+import {Todos} from './components/Todos'
+
+function App() {
+  
+
+  return (
+    <div>
+      <CreateTodo>
+
+      </CreateTodo>
+
+      <Todos>
+
+      </Todos>
+    </div>
+  )
+}
+
+export default App
